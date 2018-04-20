38b875ab72aec0df48913dc43a261aac62191e27
Afalsalah1 <afalsalah1@hawkmail.hfcc.edu>
2018-04-20 01:06:21 -0400

    it's 1AM, why am I awake?
    well, now the reports tab works as planned, there is a switch statement in the manager controller fills the reports scene as needed, it is a one scene to many JSON commands thingy, it works and it's efficient. (I think, idk, it's better than creating the same scene for multiple commands.) don't uncomment the cases that are commented until the api is complete, or do, I'm a commit message not a cop.

---
+++ src/sample/Employee.java
@@ -0,0 +1,152 @@
+package sample;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Employee {
+    private final SimpleStringProperty number;
+    private final SimpleStringProperty firstN;
+    private final SimpleStringProperty lastN;
+    private final SimpleStringProperty street;
+    private final SimpleStringProperty city;
+    private final SimpleStringProperty state;
+    private final SimpleStringProperty zip;
+    private final SimpleStringProperty phone;
+    private final SimpleStringProperty hDate;
+    private final SimpleStringProperty salary;
+    private final SimpleStringProperty bDate;
+    private final SimpleStringProperty title;
+    private final SimpleStringProperty active;
+    private final SimpleStringProperty username;
+
+    /*constructor to load up parsed JSON data in the form of objects, before being
+    loaded into the columns of the JavaFX tableview*/
+
+    public Employee(String number, String firstN, String lastN, String street, String city, String state, String zip, String phone, String hDate, String salary, String bDate, String title, String active, String username) {
+        this.number = new SimpleStringProperty(number);
+        this.firstN = new SimpleStringProperty(firstN);
+        this.lastN = new SimpleStringProperty(lastN);
+        this.street = new SimpleStringProperty(street);
+        this.city = new SimpleStringProperty(city);
+        this.state = new SimpleStringProperty(state);
+        this.zip = new SimpleStringProperty(zip);
+        this.phone = new SimpleStringProperty(phone);
+        this.hDate = new SimpleStringProperty(hDate);
+        this.salary = new SimpleStringProperty(salary);
+        this.bDate = new SimpleStringProperty(bDate);
+        this.title = new SimpleStringProperty(title);
+        this.active = new SimpleStringProperty(active);
+        this.username = new SimpleStringProperty(username);
+    }
+
+    public String getNumber() {
+        return number.get();
+    }
+
+    public SimpleStringProperty numberProperty() {
+        return number;
+    }
+
+    public String getFirstN() {
+        return firstN.get();
+    }
+
+    public SimpleStringProperty firstNProperty() {
+        return firstN;
+    }
+
+    public String getLastN() {
+        return lastN.get();
+    }
+
+    public SimpleStringProperty lastNProperty() {
+        return lastN;
+    }
+
+    public String getStreet() {
+        return street.get();
+    }
+
+    public SimpleStringProperty streetProperty() {
+        return street;
+    }
+
+    public String getCity() {
+        return city.get();
+    }
+
+    public SimpleStringProperty cityProperty() {
+        return city;
+    }
+
+    public String getState() {
+        return state.get();
+    }
+
+    public SimpleStringProperty stateProperty() {
+        return state;
+    }
+
+    public String getZip() {
+        return zip.get();
+    }
+
+    public SimpleStringProperty zipProperty() {
+        return zip;
+    }
+
+    public String getPhone() {
+        return phone.get();
+    }
+
+    public SimpleStringProperty phoneProperty() {
+        return phone;
+    }
+
+    public String gethDate() {
+        return hDate.get();
+    }
+
+    public SimpleStringProperty hDateProperty() {
+        return hDate;
+    }
+
+    public String getSalary() {
+        return salary.get();
+    }
+
+    public SimpleStringProperty salaryProperty() {
+        return salary;
+    }
+
+    public String getbDate() {
+        return bDate.get();
+    }
+
+    public SimpleStringProperty bDateProperty() {
+        return bDate;
+    }
+
+    public String getTitle() {
+        return title.get();
+    }
+
+    public SimpleStringProperty titleProperty() {
+        return title;
+    }
+
+    public String getActive() {
+        return active.get();
+    }
+
+    public SimpleStringProperty activeProperty() {
+        return active;
+    }
+
+    public String getUsername() {
+        return username.get();
+    }
+
+    public SimpleStringProperty usernameProperty() {
+        return username;
+    }
+}
--- src/sample/ManagerController.java
+++ src/sample/ManagerController.java
@@ -1,5 +1,7 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,9 +9,17 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class ManagerController {
 
@@ -43,9 +53,261 @@ public class ManagerController {
     }
 
     @FXML
-    private void rprtCustomersOnAction(ActionEvent event) throws IOException {
-        Parent reportView = FXMLLoader.load(getClass().getResource("report.fxml"));//
+    public String readJsonFromUrl(String urlString) throws IOException, JSONException {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL(urlString);
+            reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            StringBuffer buffer = new StringBuffer();
+            int read;
+            char[] chars = new char[1024];
+            while ((read = reader.read(chars)) != -1)
+                buffer.append(chars, 0, read);
+            if(buffer.equals(""))
+                buffer.append("[]");
+            return buffer.toString();
+        }
+        finally {
+            if (reader != null)
+                reader.close();
+        }
+    }
+
+    @FXML
+    private void rprtOnAction(ActionEvent event) throws IOException {
+        MenuItem miEvent = (MenuItem) event.getSource();
+        String mieID = miEvent.getId();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("report.fxml"));
+        fxmlLoader.load();
+        Parent reportView = fxmlLoader.getRoot();
         Scene reportViewScene = new Scene(reportView);
+        reportController rc = fxmlLoader.getController();
+
+        switch (mieID){
+
+            case "rprt_Customers":
+            ArrayList<Customer> list = new ArrayList<>();
+            JSONArray customers = new JSONArray(readJsonFromUrl("https://dev.cis294.hfcc.edu/api.php?username=jg23&password=I$lovedogs4433&request=getCustomer"));
+
+            TableColumn name = new TableColumn("Name");
+            TableColumn street = new TableColumn("Street");
+            TableColumn city = new TableColumn("City");
+            TableColumn state = new TableColumn("State");
+            TableColumn zip = new TableColumn("ZIP");
+            TableColumn id = new TableColumn("ID");
+            TableColumn email = new TableColumn("Email");
+            TableColumn phone = new TableColumn("Phone");
+            rc.setTableColumns(name, street, city, state, zip, id, email, phone);
+
+            for (int i = 0; i < customers.length(); i++) {
+                JSONObject customer = customers.getJSONObject(i);
+                System.out.println(customer);
+                String strName = customer.getString("Cus_Name");
+                String strStreet = customer.getString("Cus_StreetAddress");
+                String strCity = customer.getString("Cus_City");
+                String strState = customer.getString("Cus_State");
+                String strZIP = customer.getString("Cus_ZipCode");
+                String strID = customer.getString("Cus_CustomerNumber");
+                String strEmail = customer.getString("Cus_Email");
+                String strPhone = customer.getString("Cus_PhoneNumber");
+                list.add(new Customer(strName, strStreet, strCity, strState, strZIP, strPhone, strEmail, strID));
+            }
+            System.out.println(list);
+            ObservableList<Customer> obList = FXCollections.observableArrayList(list);
+            name.setCellValueFactory(new PropertyValueFactory<Customer, String>("name"));
+            street.setCellValueFactory(new PropertyValueFactory<Customer, String>("street"));
+            city.setCellValueFactory(new PropertyValueFactory<Customer, String>("city"));
+            state.setCellValueFactory(new PropertyValueFactory<Customer, String>("state"));
+            zip.setCellValueFactory(new PropertyValueFactory<Customer, String>("zip"));
+            id.setCellValueFactory(new PropertyValueFactory<Customer, String>("id"));
+            email.setCellValueFactory(new PropertyValueFactory<Customer, String>("email"));
+            phone.setCellValueFactory(new PropertyValueFactory<Customer, String>("phone"));
+
+            rc.setTable(obList);
+            break;
+
+            /*case "rprt_Contracts":
+                ArrayList<Customer> list = new ArrayList<>();
+                JSONArray customers = new JSONArray(readJsonFromUrl("https://dev.cis294.hfcc.edu/api.php?username=jg23&password=I$lovedogs4433&request=getCustomer"));
+
+                TableColumn name = new TableColumn("Name");
+                TableColumn street = new TableColumn("Street");
+                TableColumn city = new TableColumn("City");
+                TableColumn state = new TableColumn("State");
+                TableColumn zip = new TableColumn("ZIP");
+                TableColumn id = new TableColumn("ID");
+                TableColumn email = new TableColumn("Email");
+                TableColumn phone = new TableColumn("Phone");
+                rc.setTableColumns(name, street, city, state, zip, id, email, phone);
+
+                for (int i = 0; i < customers.length(); i++) {
+                    JSONObject customer = customers.getJSONObject(i);
+                    System.out.println(customer);
+                    String strName = customer.getString("Cus_Name");
+                    String strStreet = customer.getString("Cus_StreetAddress");
+                    String strCity = customer.getString("Cus_City");
+                    String strState = customer.getString("Cus_State");
+                    String strZIP = customer.getString("Cus_ZipCode");
+                    String strID = customer.getString("Cus_CustomerNumber");
+                    String strEmail = customer.getString("Cus_Email");
+                    String strPhone = customer.getString("Cus_PhoneNumber");
+                    list.add(new Customer(strName, strStreet, strCity, strState, strZIP, strPhone, strEmail, strID));
+                }
+                System.out.println(list);
+                ObservableList<Customer> obList = FXCollections.observableArrayList(list);
+                name.setCellValueFactory(new PropertyValueFactory<Customer, String>("name"));
+                street.setCellValueFactory(new PropertyValueFactory<Customer, String>("street"));
+                city.setCellValueFactory(new PropertyValueFactory<Customer, String>("city"));
+                state.setCellValueFactory(new PropertyValueFactory<Customer, String>("state"));
+                zip.setCellValueFactory(new PropertyValueFactory<Customer, String>("zip"));
+                id.setCellValueFactory(new PropertyValueFactory<Customer, String>("id"));
+                email.setCellValueFactory(new PropertyValueFactory<Customer, String>("email"));
+                phone.setCellValueFactory(new PropertyValueFactory<Customer, String>("phone"));
+
+                rc.setTable(obList);
+                break;*/
+
+            case "rprt_Employees":
+                ArrayList<Employee> elist = new ArrayList<>();
+                JSONArray employees = new JSONArray(readJsonFromUrl("https://dev.cis294.hfcc.edu/api.php?username=jg23&password=I$lovedogs4433&request=getEmployee"));
+
+                TableColumn eID = new TableColumn("ID");
+                TableColumn fName = new TableColumn("First Name");
+                TableColumn lName = new TableColumn("Last Name");
+                TableColumn eStreet = new TableColumn("Street");
+                TableColumn eCity = new TableColumn("City");
+                TableColumn eState = new TableColumn("State");
+                TableColumn eZip = new TableColumn("ZIP");
+                TableColumn ePhone = new TableColumn("Phone");
+                TableColumn eHire = new TableColumn("Hire Date");
+                TableColumn eSalary = new TableColumn("Salary");
+                TableColumn eBirth = new TableColumn("Birth Date");
+                TableColumn eTitle = new TableColumn("Title");
+                TableColumn eActive = new TableColumn("Active");
+                TableColumn eUser = new TableColumn("UserName");
+                rc.setTableColumns(eID, fName, lName, eStreet, eCity, eState, eZip, ePhone, eHire, eSalary, eBirth, eTitle, eActive, eUser);
+
+                for (int i = 0; i < employees.length(); i++) {
+                    JSONObject employee = employees.getJSONObject(i);
+                    System.out.println(employee);
+                    String strEID = employee.getString("Emp_EmployeeNumber");
+                    String strEFN = employee.getString("Emp_FirstName");
+                    String strELN = employee.getString("Emp_LastName");
+                    String strESTRT = employee.getString("Emp_StreetAddress");
+                    String strECTY = employee.getString("Emp_City");
+                    String strESTT = employee.getString("Emp_State");
+                    String strEZIP = employee.getString("Emp_ZipCode");
+                    String strEPHN = employee.getString("Emp_PhoneNumber");
+                    String strEHRE = employee.getString("Emp_HireDate");
+                    String strESAL = employee.getString("Emp_Salary");
+                    String strEBRTH = employee.getString("Emp_BirthDate");
+                    String strET = employee.getString("Emp_JobTitle");
+                    String strEACT = employee.getString("Emp_Active");
+                    String strEUSR = employee.getString("Emp_Username");
+                    elist.add(new Employee(strEID, strEFN, strELN, strESTRT, strECTY, strESTT, strEZIP, strEPHN, strEHRE, strESAL, strEBRTH, strET, strEACT, strEUSR));
+                }
+                ObservableList<Employee> eobList = FXCollections.observableArrayList(elist);
+                eID.setCellValueFactory(new PropertyValueFactory<Customer, String>("number"));
+                fName.setCellValueFactory(new PropertyValueFactory<Customer, String>("firstN"));
+                lName.setCellValueFactory(new PropertyValueFactory<Customer, String>("lastN"));
+                eStreet.setCellValueFactory(new PropertyValueFactory<Customer, String>("street"));
+                eCity.setCellValueFactory(new PropertyValueFactory<Customer, String>("city"));
+                eState.setCellValueFactory(new PropertyValueFactory<Customer, String>("state"));
+                eZip.setCellValueFactory(new PropertyValueFactory<Customer, String>("zip"));
+                ePhone.setCellValueFactory(new PropertyValueFactory<Customer, String>("phone"));
+                eHire.setCellValueFactory(new PropertyValueFactory<Customer, String>("hDate"));
+                eSalary.setCellValueFactory(new PropertyValueFactory<Customer, String>("salary"));
+                eBirth.setCellValueFactory(new PropertyValueFactory<Customer, String>("bDate"));
+                eTitle.setCellValueFactory(new PropertyValueFactory<Customer, String>("title"));
+                eActive.setCellValueFactory(new PropertyValueFactory<Customer, String>("active"));
+                eUser.setCellValueFactory(new PropertyValueFactory<Customer, String>("username"));
+
+                rc.setTable(eobList);
+                break;
+
+           /* case "rprt_Invoices":
+                ArrayList<Customer> list = new ArrayList<>();
+                JSONArray customers = new JSONArray(readJsonFromUrl("https://dev.cis294.hfcc.edu/api.php?username=jg23&password=I$lovedogs4433&request=getCustomer"));
+
+                TableColumn name = new TableColumn("Name");
+                TableColumn street = new TableColumn("Street");
+                TableColumn city = new TableColumn("City");
+                TableColumn state = new TableColumn("State");
+                TableColumn zip = new TableColumn("ZIP");
+                TableColumn id = new TableColumn("ID");
+                TableColumn email = new TableColumn("Email");
+                TableColumn phone = new TableColumn("Phone");
+                rc.setTableColumns(name, street, city, state, zip, id, email, phone);
+
+                for (int i = 0; i < customers.length(); i++) {
+                    JSONObject customer = customers.getJSONObject(i);
+                    System.out.println(customer);
+                    String strName = customer.getString("Cus_Name");
+                    String strStreet = customer.getString("Cus_StreetAddress");
+                    String strCity = customer.getString("Cus_City");
+                    String strState = customer.getString("Cus_State");
+                    String strZIP = customer.getString("Cus_ZipCode");
+                    String strID = customer.getString("Cus_CustomerNumber");
+                    String strEmail = customer.getString("Cus_Email");
+                    String strPhone = customer.getString("Cus_PhoneNumber");
+                    list.add(new Customer(strName, strStreet, strCity, strState, strZIP, strPhone, strEmail, strID));
+                }
+                System.out.println(list);
+                ObservableList<Customer> obList = FXCollections.observableArrayList(list);
+                name.setCellValueFactory(new PropertyValueFactory<Customer, String>("name"));
+                street.setCellValueFactory(new PropertyValueFactory<Customer, String>("street"));
+                city.setCellValueFactory(new PropertyValueFactory<Customer, String>("city"));
+                state.setCellValueFactory(new PropertyValueFactory<Customer, String>("state"));
+                zip.setCellValueFactory(new PropertyValueFactory<Customer, String>("zip"));
+                id.setCellValueFactory(new PropertyValueFactory<Customer, String>("id"));
+                email.setCellValueFactory(new PropertyValueFactory<Customer, String>("email"));
+                phone.setCellValueFactory(new PropertyValueFactory<Customer, String>("phone"));
+
+                rc.setTable(obList);
+                break;
+
+            case "rprt_Tickets":
+                ArrayList<Customer> list = new ArrayList<>();
+                JSONArray customers = new JSONArray(readJsonFromUrl("https://dev.cis294.hfcc.edu/api.php?username=jg23&password=I$lovedogs4433&request=getCustomer"));
+
+                TableColumn name = new TableColumn("Name");
+                TableColumn street = new TableColumn("Street");
+                TableColumn city = new TableColumn("City");
+                TableColumn state = new TableColumn("State");
+                TableColumn zip = new TableColumn("ZIP");
+                TableColumn id = new TableColumn("ID");
+                TableColumn email = new TableColumn("Email");
+                TableColumn phone = new TableColumn("Phone");
+                rc.setTableColumns(name, street, city, state, zip, id, email, phone);
+
+                for (int i = 0; i < customers.length(); i++) {
+                    JSONObject customer = customers.getJSONObject(i);
+                    System.out.println(customer);
+                    String strName = customer.getString("Cus_Name");
+                    String strStreet = customer.getString("Cus_StreetAddress");
+                    String strCity = customer.getString("Cus_City");
+                    String strState = customer.getString("Cus_State");
+                    String strZIP = customer.getString("Cus_ZipCode");
+                    String strID = customer.getString("Cus_CustomerNumber");
+                    String strEmail = customer.getString("Cus_Email");
+                    String strPhone = customer.getString("Cus_PhoneNumber");
+                    list.add(new Customer(strName, strStreet, strCity, strState, strZIP, strPhone, strEmail, strID));
+                }
+                System.out.println(list);
+                ObservableList<Customer> obList = FXCollections.observableArrayList(list);
+                name.setCellValueFactory(new PropertyValueFactory<Customer, String>("name"));
+                street.setCellValueFactory(new PropertyValueFactory<Customer, String>("street"));
+                city.setCellValueFactory(new PropertyValueFactory<Customer, String>("city"));
+                state.setCellValueFactory(new PropertyValueFactory<Customer, String>("state"));
+                zip.setCellValueFactory(new PropertyValueFactory<Customer, String>("zip"));
+                id.setCellValueFactory(new PropertyValueFactory<Customer, String>("id"));
+                email.setCellValueFactory(new PropertyValueFactory<Customer, String>("email"));
+                phone.setCellValueFactory(new PropertyValueFactory<Customer, String>("phone"));
+
+                rc.setTable(obList);
+                break;*/
+        }
+
 
         Stage window;
         window = (Stage) (root.getScene().getWindow());
